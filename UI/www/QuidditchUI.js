761eb565e8aef14c4110d10f3f3e1bc0632f3d3d
Dr-Turtle <reeddwight1@gmail.com>
2017-12-16 14:57:24 -0500

    added score buttons

--- UI/www/QuidditchUI.js
+++ UI/www/QuidditchUI.js
@@ -6,3 +6,19 @@ $('#start').on('click', function() {
 $('#stop').on('click', function() {
 	NetworkTables.putValue("/SmartDashboard/controls_enabled", false);
 });
+
+var score = 0;
+
+$('.hoop button').on('click', function() {
+	var button = $(this);
+	var targetClass = button.data('target');
+	var targetElement = button.parent().find("." + targetClass);
+	var buttonValue = button.data("value");
+	var currentTargetValue = parseInt(targetElement.text());
+	var nextTargetValue = currentTargetValue + (buttonValue > 0 ? 1 : -1);
+	if (currentTargetValue > 0 || buttonValue > 0){
+		targetElement.text(nextTargetValue);
+		score = (score+button.data("value"));
+		$(".score").text(score);
+	}
+});
