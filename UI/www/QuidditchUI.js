788f59d46921a724dc8b66928255dd4f60c1ba38
David <daviddanielian3@gmail.com>
2017-12-16 14:56:17 -0500

    timer

--- UI/www/QuidditchUI.js
+++ UI/www/QuidditchUI.js
@@ -1,20 +1,33 @@
+var match_length = 5000;
+var start_button_time = match_length;
+var change_time;
+showTime(start_button_time);
 
 $('#start').on('click', function() {
-	//NetworkTables.putValue("/SmartDashboard/controls_enabled", true);
+	NetworkTables.putValue("/SmartDashboard/controls_enabled", true);
 	var startTime = Date.now();
-	setInterval(function() {
-		showTime(startTime);
-	}, 100);
+	change_time = setInterval(function() {
+			showTime(start_button_time);
+			if(start_button_time <= 0) {
+				NetworkTables.putValue("/SmartDashboard/controls_enabled", false);
+				clearInterval(change_time);
+			}
+			start_button_time -= 100;
+		}, 100);
 });
 
 $('#stop').on('click', function() {
-	//NetworkTables.putValue("/SmartDashboard/controls_enabled", false);
+	NetworkTables.putValue("/SmartDashboard/controls_enabled", false);
+	clearInterval(change_time);
 });
 
-function showTime(time) {
-		var timeElapsed = Date.now() - time;
-		var timeLeft = 135000 - timeElapsed;
-		
+$('#reset').on('click', function() {
+	start_button_time = match_length;
+	showTime(start_button_time);
+});
+
+function showTime(timeLeft) {	
+		console.log(timeLeft)
 		var seconds = parseInt((timeLeft / 1000) % 60);
 		var minutes = parseInt((timeLeft / 1000) / 60);
 		
@@ -27,5 +40,6 @@ function showTime(time) {
 			$('.seconds').text(seconds);
 			
 }
+
 
 
