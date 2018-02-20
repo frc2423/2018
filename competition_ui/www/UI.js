73d04320853ebb506fe52322ec01aa25fd68e0f0
David <daviddanielian3@gmail.com>
2018-02-19 22:28:52 -0500

    auto mode buttons now send the network table keys

--- competition_ui/www/UI.js
+++ competition_ui/www/UI.js
@@ -1,3 +1,11 @@
-$("input[name='position']").on("change", function() {
-	alert($("input[name='position']").val())
+$("input[name='position']").change( function() {
+	NetworkTables.putValue("/autonomous/DriveNDrop/robot_position", $("input[name='position']:checked").val())
+});
+
+$("input[name='left_switch']").change( function() {
+	NetworkTables("/autonomous/DriveNDrop/action_if_left_switch", $("input[name='left_switch']:checked").val())
+});
+
+$("input[name='right_switch']").change( function() {
+	NetworkTables("/autonomous/DriveNDrop/action_if right_switch", $("input[name='right_switch']:checked").val())
 });
