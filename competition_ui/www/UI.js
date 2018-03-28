5569a9cbf6fd086c22d864aec5b6755214a964b8
David <daviddanielian3@gmail.com>
2018-03-27 21:02:29 -0400

    Canvas example code working

--- competition_ui/www/UI.js
+++ competition_ui/www/UI.js
@@ -10,3 +10,17 @@ $("input[name='right_switch']").change( function() {
 	NetworkTables.putValue("/autonomous/DriveNDrop/action_if right_switch", $("input[name='right_switch']:checked").val())
 });
 
+// create a wrapper around native canvas element (with id="c")
+var canvas = new fabric.Canvas('c');
+
+// create a rectangle object
+var rect = new fabric.Rect({
+left: 100,
+top: 100,
+fill: 'red',
+width: 20,
+height: 20
+});
+
+// "add" rectangle onto canvas
+canvas.add(rect);
